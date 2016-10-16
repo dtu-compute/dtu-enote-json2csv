d65469c957cf2fd476bdec4084a303f44a43efa7
Iain Bryson <iain@iain-bryson.ca>
2016-10-16 12:57:45 -0700

    Update for latest export format

--- json2csv.R
+++ json2csv.R
@@ -1,27 +1,35 @@
 library(jsonlite)
+library(lubridate)
 
-js <- stream_in(con=file("~/Downloads/export.json"))
+json_file <- "~/Downloads/18.json"
+
+#js <- stream_in(con=file(json_file))
+#js <- fromJSON(sprintf("[%s]", paste(readLines(json_file), collapse=",")))
+js <- fromJSON(sprintf("%s", paste(readLines(json_file))))
 js
 
 str(js)
 js$meta
-names(js$submissions[[1]])
+names(js$submissions)
 
-
-js$submissions[[1]][["Student ID"]]
-js$submissions[[1]][["Attempt No"]]
-length(js$submissions[[1]]$questions)
-
-(js$submissions[[1]]$questions[[10]])
-(js$submissions[[1]]$questions[[8]])
 
 # Note that counters start from 0. E.g. "Question Students Answer"
 
 ## Collecting the answers
-answers <- js$submissions[[1]][,c("Student ID", "Student Name", "Attempt No", "Quiz Attempt Number", "Time Used" )]
-questions <- t(sapply(js$submissions[[1]]$questions,function(x) x[["Question Is Correct"]]))
+answers <- js$submissions[,c("Student ID", "Student Name", "Attempt No", "Quiz Attempt Number", "Time Used" )]
+answers[, "Time Used"] <- lubridate:::.strptime(js$submissions[,"Last Edit Time"], "%Y-%m-%dT%H:%M:%OS%OO") - lubridate:::.strptime(js$submissions[,"First Edit Time"], "%Y-%m-%dT%H:%M:%OS%OO")
+questions <- t(sapply(js$submissions$questions,function(x) x[["Question Is Correct"]]))
 questions <- as.data.frame(questions)
 names(questions) <- paste0("Q", 1:ncol(questions))
+qstats = apply(questions[,], 2, function(x) length(which(x))) / nrow(questions[,])
+questions$TotalCorrect = rowSums(questions)
+questions$PctCorrect = 100.0(questions$TotalCorrect / (ncol(questions)-1))
 answers <- cbind(answers, questions)
 
-write.table(answers, file="~/teaching/IntroStat/test_quiz.csv", sep=",", row.names = FALSE)
+write.table(answers, file="~/Downloads/test_quiz.csv", sep=",", row.names = FALSE)
+
+print(answers)
+
+## Question stats
+
+print(qstats)
