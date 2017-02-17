f7089cabeebdcae3529d31cb622d0fef4a1bf438
Iain Bryson <bryson.iain@gmail.com>
2017-02-17 08:02:17 -0800

    _really_ get the arguments

--- json2csv.R
+++ json2csv.R
@@ -3,6 +3,8 @@ library(lubridate)
 
 json_file <- "~/Downloads/18.json"
 
+args = commandArgs(trailingOnly=TRUE)
+
 if (length(args)!=1) {
   stop("At least one argument must be supplied (input file).n", call.=FALSE)
 } else if (length(args)==1) {
